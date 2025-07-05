0edd04c60feeba76c434dfbb53ddba4c508fa580
fcursino <fabriciocursino09@gmail.com>
2025-07-05 11:16:47 -0300

    adjusting project structure in the main folder

---
+++ src/main/java/com/stock/faberfarma/FaberfarmaApplication.java
@@ -0,0 +1,13 @@
+package com.stock.faberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaberfarmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaberfarmaApplication.class, args);
+    }
+
+}
